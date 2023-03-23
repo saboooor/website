b5d758a78f68d4ba73d8ab24e5477afa490c812a
saboooor <saboor547@gmail.com>
2023-03-22 22:17:32 -0600

    Revert "add redirect"
    This reverts commit d56393fdd7fd2f22ed1d1c8f7ec17a1bec6e1666.

--- src/routes/[...404]/index.tsx
+++ src/routes/[...404]/index.tsx
@@ -1,11 +1,45 @@
-export const onGet: any = async ({ redirect, url }: any) => {
-  if (url.pathname === '/resume') {
-    throw redirect(301, `https://sab.luminescent.dev/resume`);
-  }
-  else if (url.pathname.startsWith('/transcript')) {
-    throw redirect(301, `https://transcript.luminescent.dev${url.pathname.replace('/transcript', '')}`)
-  }
-  else {
-    throw redirect(301, `https://luminescent.dev${url.pathname}`)
-  }
-};
+import { component$ } from '@builder.io/qwik';
+import { DocumentHead, Link } from '@builder.io/qwik-city';
+
+import { ChevronLeftIcon } from "qwik-feather-icons";
+
+export default component$(() => {
+  return (
+    <section class="flex mx-auto max-w-6xl px-6 items-center" style={{ minHeight: 'calc(100dvh - 64px)' }}>
+      <div class="text-left space-y-5">
+        <div class="flex sm:hidden relative justify-start sm:ml-auto mb-5" style="width: 100%;">
+          <img class="rounded-2xl ease-in-out filter drop-shadow-2xl" src={`https://avatars.githubusercontent.com/u/42164502`} style={{ maxWidth: '50%' }} alt="sab's pfp" />
+        </div>
+        <h1 class="font-bold text-red-500 text-2xl sm:text-5xl">
+          404: Page not found
+        </h1>
+        <p class="text-lg sm:text-2xl text-gray-400">
+          Whoops! You've hit a dead-end. 
+        </p>
+        <div class="grid sm:flex gap-3 pt-4">
+          <Link href="/" class="flex justify-center sm:justify-start transition duration-200 rounded-2xl shadow-lg backdrop-blur-lg bg-purple-900/80 hover:bg-purple-900 border-2 border-purple-900 px-6 py-3 font-bold text-purple-100 md:py-4 md:px-8 text-sm md:text-lg whitespace-nowrap gap-4 items-center">
+            <ChevronLeftIcon/> Go back home
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+});
+
+export const head: DocumentHead = {
+  title: '404',
+  meta: [
+    {
+      name: 'description',
+      content: 'Welcome to sab\'s website idk'
+    },
+    {
+      name: 'og:description',
+      content: 'Welcome to sab\'s website idk'
+    },
+    {
+      name: 'og:image',
+      content: 'https://avatars.githubusercontent.com/u/42164502'
+    }
+  ]
+}
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,8 +1,10 @@
 import { component$, Slot } from '@builder.io/qwik';
+import Nav from '../components/Nav';
 
 export default component$(() => {
   return (
     <main>
+      <Nav />
       <section class="pt-16">
         <Slot />
       </section>
