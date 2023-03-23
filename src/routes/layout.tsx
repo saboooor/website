d56393fdd7fd2f22ed1d1c8f7ec17a1bec6e1666
saboooor <saboor547@gmail.com>
2023-03-22 22:14:11 -0600

    add redirect

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,8 @@
 import { component$, Slot } from '@builder.io/qwik';
-import Nav from '../components/Nav';
 
 export default component$(() => {
   return (
     <main>
-      <Nav />
       <section class="pt-16">
         <Slot />
       </section>
